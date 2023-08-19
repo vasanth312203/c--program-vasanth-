4ffcd301e2c7177f2e4c37a2b93eb62c925d3e16
vasanth312203 <113229289+vasanth312203@users.noreply.github.com>
2023-08-19 20:49:01 +0530

    Add files via upload

---
+++ assignment 1.cpp
@@ -0,0 +1,20 @@
+#include<iostream>
+
+
+using namespace std;
+
+int main(){
+
+int num;
+cout<<"Enter the number :\t";
+cin>>num;
+if(num %2 == 0){
+    cout<<"Number is Even ";
+
+}
+else{
+    cout<<"Number is Odd";
+
+}
+return 0;
+}
